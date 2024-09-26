636786a2891084a46ace9621cde98461a79f8941
GiovanePS <giovanespace@gmail.com>
2024-09-25 21:20:53 -0300

    Ex 01 resolvido

--- exercicio2/main.c
+++ exercicio2/main.c
@@ -1,9 +1,11 @@
+#include <math.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 #define NUM_THREADS 8
+#define TAM_NUM_SORTE 10000000
 
 int numSorte;
 pthread_cond_t condition;
@@ -11,24 +13,36 @@ pthread_mutex_t lock;
 
 void *functionAuditora() {
   pthread_mutex_lock(&lock);
-  numSorte = rand() % 100 + 1;
-  pthread_cond_wait(&condition, &lock);
+  numSorte = rand() % TAM_NUM_SORTE + 1;
+  while (numSorte != 0) {
+    pthread_cond_wait(&condition, &lock);
+  }
+
   pthread_mutex_unlock(&lock);
   return NULL;
 }
 
 void *functionApostadora() {
+  int contadorDeChutes = 0;
   int chute = rand() % 100 + 1;
   while (1) {
+    contadorDeChutes++;
     if (chute == numSorte) {
+      pthread_mutex_lock(&lock);
+      numSorte = 0;
       printf("Achei o número da sorte!\n");
+      printf("Eu sou a thread nº %lu\n", pthread_self());
+      printf("Achei na tentativa nº %d\n", contadorDeChutes);
+      printf( "O número da sorte é %d\n", chute);
       pthread_cond_signal(&condition);
+      pthread_mutex_unlock(&lock);
       return NULL;
     }
 
-    chute++;
-    if (chute > 100) {
-      chute = rand() % 100 + 1;
+    chute = rand() % TAM_NUM_SORTE + 1;
+
+    if (numSorte == 0) {
+      return NULL;
     }
   }
 }
